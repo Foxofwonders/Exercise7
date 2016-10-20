f076fe75276a1c7b70b2fe1279787f12d8342592
Ysbrand van Eijck <ysconator@hotmail.com>
2016-10-20 13:07:12 +0200

    Initial files

---
+++ src/nl/ru/ai/annadenise/exercise7/Concordances.java
@@ -0,0 +1,22 @@
+/**
+ * Description of application/ class.
+ */
+package nl.ru.ai.annadenise.exercise7;
+
+/**
+ * @author a (s000)
+ * @author a (s000)
+ */
+public class Concordances
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, world!");
+
+  }
+
+}
